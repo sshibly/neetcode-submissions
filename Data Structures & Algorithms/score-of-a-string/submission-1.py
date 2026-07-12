f1d426a78c3de47468833de3f4086064a7099dc5
sshibly <sshibly17vt@gmail.com>
2026-07-12 12:04:30 -0400

    Add: score-of-a-string - submission-1

---
+++ Data Structures & Algorithms/score-of-a-string/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        i = 0
+        j = 1
+        score = 0
+
+        while j < len(s):
+            score += abs(ord(s[j]) - ord(s[i]))
+            j += 1
+            i += 1
+        
+        return score
+
+        # time complexity: O(n)
+        # space complexity: O(1)
+
+        
