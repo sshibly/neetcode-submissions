1dc3cd056095b7250ed48a60bdbdd61532dcde6d
sshibly <sshibly17vt@gmail.com>
2026-09-01 23:06:38 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} # val -> index
+
+        for i, val in enumerate(nums):
+            diff = target - val
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[val] = i
+        
+        return []
+
+        # time complexity: O(n)
+        # space complexity: O(n)
